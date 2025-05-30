3e3a5bddf0b01b3fe22522afa39b8876c761106b
caoh2 <cao970109@gmail.com>
2025-05-31 01:21:06 +0800

    feat(infrastructure): 添加 Redis、S3 和锁定模块配置
    - 在 .env.template 中添加了新的环境变量配置
    - 更新 medusa-config.ts 以支持 Redis 事件总线、S3 文件存储和 Redis 锁定模块
    - 移除了 apps/backend/package.json 中的端口参数
    - 更新了 HttpClient 类中的查询字符串处理方式

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -14,7 +14,7 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
     },
-    // redisUrl: process.env.REDIS_URL
+    redisUrl: process.env.REDIS_URL
   },
   modules: [
     { resolve: './src/modules/seller' },
@@ -79,6 +79,54 @@ module.exports = defineConfig({
             id: 'local',
             options: {
               channels: ['feed']
+            }
+          }
+        ]
+      }
+    },
+    // https://docs.medusajs.com/resources/infrastructure-modules/event
+    {
+      resolve: '@medusajs/medusa/event-bus-redis',
+      options: {
+        redisUrl: process.env.EVENTS_REDIS_URL
+      }
+    },
+    // https://docs.medusajs.com/resources/infrastructure-modules/file/s3
+    {
+      resolve: '@medusajs/medusa/file',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/medusa/file-s3',
+            id: 's3',
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              additional_client_config: {
+                // forcePathStyle: true // Additional Configuration for MinIO and Supabase#
+              }
+            }
+          }
+        ]
+      }
+    },
+    // https://docs.medusajs.com/resources/infrastructure-modules/locking/redis
+    {
+      resolve: '@medusajs/medusa/locking',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/medusa/locking-redis',
+            id: 'locking-redis',
+            // set this if you want this provider to be used by default
+            // and you have other Locking Module Providers registered.
+            is_default: true,
+            options: {
+              redisUrl: process.env.LOCKING_REDIS_URL
             }
           }
         ]
